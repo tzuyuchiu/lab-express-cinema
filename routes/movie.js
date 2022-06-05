36874bc3c9885dff6159b73ef4fc570270431ff4
Tzu Yu Chiu <tzuyujulia@gmail.com>
2022-06-05 11:04:16 +0200

    done

--- routes/movie.js
+++ routes/movie.js
@@ -1,16 +1,25 @@
 const express = require('express');
+const { request } = require('../app.js');
 const router = express.Router();
 
-const Movie = require('../models/Move.model');
-
+const Movie = require('../models/Movie.model.js');
+//Iteration 2 | List the Movies
 router.get('/movies', async (req, res, next) => {
   try {
-    const findListOfMovie = await Movie.find();
-    //select('title', 'director');
+    const findListOfMovie = await Movie.find({}).select('title director');
     res.status(200).json(findListOfMovie);
   } catch (error) {
     next(error);
   }
 });
-
+//Iteration 3 | Display Movie Details
+router.get('/movies/:id', async (req, res, next) => {
+  try {
+    movieId = req.params.id;
+    const findOneMovie = await Movie.findByIdAndUpdate(movieId, req.body);
+    res.json(findOneMovie);
+  } catch (error) {
+    next(error);
+  }
+});
 module.exports = router;
